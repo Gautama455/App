af561395a8cc043d6056c8b6d0cb73d086c7b4cf
Gautama455 <water.universe@yandex.ru>
2026-06-16 17:16:19 +0300

    Добавил поле к api-контроллеру

--- src/App.WebApi/Controllers/ValidationController.cs
+++ src/App.WebApi/Controllers/ValidationController.cs
@@ -4,34 +4,62 @@ using Microsoft.AspNetCore.Mvc;
 using App.ValidationAccess.Requests;
 using FluentValidation;
 using App.ValidationAccess.Services;
-using System.ComponentModel.DataAnnotations;
+using Npgsql.Internal.Postgres;
 
 [ApiController]
 [Route("api/validation")]
 public class ValidationController : ControllerBase
 {
     private IUserRepository _repo;
-    private IValidator<LoginRequest> _validator;
+    private IValidator<LoginRequest> _loginValidator;
+    private IValidator<RegisterRequest> _registerValidator;
     private IPasswordHasher _hasher;
 
     public ValidationController
         (
             IUserRepository repo,
-            IValidator<LoginRequest> validator,
+            IValidator<LoginRequest> loginValidator,
+            IValidator<RegisterRequest> registerValidator,
             IPasswordHasher hasher
         )
-    { _repo = repo; _validator = validator; _hasher = hasher; }
+    { _repo = repo; _loginValidator = loginValidator; _registerValidator = registerValidator; _hasher = hasher; }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginRequest request)
     {
-        var isValid = await _validator.ValidateAsync(request);
+        var isValid = await _loginValidator.ValidateAsync(request);
         if (!isValid.IsValid) return BadRequest(isValid.Errors);
 
         var user = await _repo.GetByLoginAsync(request.Login);
 
         if (user is null) return Unauthorized("Пользователь не найден");
         if (!_hasher.Verify(request.Password, user.Password_hash)) return Unauthorized("Неверный пароль");
-        return Ok(new {Message = "Неверный пароль"});
+        return Ok(new { Message = "Неверный пароль" });
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var isValid = await _registerValidator.ValidateAsync(request);
+
+        if (!isValid.IsValid) return BadRequest(isValid.Errors);
+
+        if (await _repo.LoginExistAsync(request.Login)) return Conflict(new { Field = "Login", Message = "Логин уже используется" });
+        if (await _repo.EmailExistAsync(request.Email)) return Conflict(new { Field = "Email", Message = "Email уже используется" });
+
+        string passwordHash = _hasher.Hash(request.Password);
+
+        await _repo.CreateAsync
+        (
+            new UserDBModel
+            {
+                Name = request.Name,
+                Login = request.Login,
+                Password_hash = passwordHash,
+                Email = request.Email
+            }
+        );
+
+        return Ok(new { Message = "Регистрация успешна" });
     }
 }
