9b257e5b46b2dabc7fb93045f4b438fe0351a4a6
Gautama455 <water.universe@yandex.ru>
2026-06-16 16:06:59 +0300

    убрал лишние юзинги

--- src/App.WebApi/Controllers/UserController.cs
+++ src/App.WebApi/Controllers/UserController.cs
@@ -2,8 +2,6 @@ using App.DataAccess.Entities.ViewModel;
 using App.DataAccess.Entities.DBModel;
 using App.DataAccess.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using App.ValidationAccess.Services;
-using FluentValidation;
 
 [ApiController]
 [Route("api/users")]
