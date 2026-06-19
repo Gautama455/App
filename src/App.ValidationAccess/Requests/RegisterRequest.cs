20a9b6c31b64f75f10019086c1f5ab1a5f156dc5
Gautama455 <water.universe@yandex.ru>
2026-06-19 12:20:12 +0300

    переместил все реквесты в отдельный файл

--- src/App.ValidationAccess/Requests/RegisterRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace App.ValidationAccess.Requests;
-
-public record RegisterRequest(string Name,string Login, string Email, string Password);
--- src/App.ValidationAccess/Requests/Requests.cs
+++ src/App.ValidationAccess/Requests/Requests.cs
@@ -1,3 +1,4 @@
 namespace App.ValidationAccess.Requests;
 
+public record RegisterRequest(string Name,string Login, string Email, string Password);
 public record LoginRequest(string Login, string Password);
